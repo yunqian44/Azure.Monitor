bad8e0f2e35c410c1d690731b6ea699414702f61
yunqian44 <yunqian8@live.com>
2020-06-22 14:44:13 +0800

    添加Application Insights

--- Azure.Monitor.Application_Insights/Startup.cs
+++ Azure.Monitor.Application_Insights/Startup.cs
@@ -23,6 +23,7 @@ namespace Azure.Monitor.Application_Insights
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
