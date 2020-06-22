715f4bc27ad72fb34259c2aabd5e2c39c7abf17a
yunqian44 <yunqian8@live.com>
2020-06-22 14:22:21 +0800

    添加项目文件。

---
+++ Azure.Monitor.Application_Insights/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure.Monitor.Application_Insights.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
